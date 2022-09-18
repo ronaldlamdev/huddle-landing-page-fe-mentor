32ac7c84f7ee6083705ae98afd9efe75648e0c0c
milessteamaccount <ronaldlamdev@gmail.com>
2022-09-17 20:08:38 -0500

    finished main and maincard component desktop styling

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const MainCard = ({image, forAlt, heading, para}) => {
   return (
-    <div className='bg-white rounded-lg shadow-xl flex flex-col justify-between items-center text-center p-8 mx-auto gap-8'>
-      <img src={image} alt={forAlt}/>
+    <div className='bg-white rounded-lg shadow-xl flex flex-col justify-between items-center text-center p-8 mx-auto gap-8 lg:flex-row w-full main-card'>
+      <img className='lg:w-[50%]' src={image} alt={forAlt}/>
       <div className='flex flex-col justify-between gap-4'>
-        <h1 className='text-2xl'>{heading}</h1>
-        <p className='text-[#808d99] text-lg'>{para}</p>
+        <h1 className='text-2xl lg:text-3xl'>{heading}</h1>
+        <p className='text-[#808d99] text-lg lg:text-xl'>{para}</p>
       </div>
     </div>
   )
